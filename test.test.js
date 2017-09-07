5dc9faa44aa16b3390b0816bed5927847c6ca318
Duart <duartbreedt@gmail.com>
2017-09-07 21:54:28 +0200

    feat(Testing): Failed attempt

--- test.test.js
+++
@@ -1,7 +0,0 @@
-describe('sorting the list of users', function() {
-  it('sorts in descending order by default', function() {
-    var users = ['jack', 'igor', 'jeff'];
-    var sorted = "jack";
-    expect(sorted).toEqual("jack");
-  });
-});
